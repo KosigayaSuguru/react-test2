1d9042c8709cbc0b9bbd2091a2dc209e53bff244
self2 <self2>
2019-12-02 00:00:51 +0900

    ESLint整形

--- src/components/my/contents1/index.js
+++ src/components/my/contents1/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 
 export default class Contents1 extends React.Component {
     render() {
         return (
-            <div style={{ flex: "1 1 0%", padding: "10px" }}>
+            <div style={{ flex: '1 1 0%', padding: '10px' }}>
                 contents11
             </div>
-        )
+        );
     }
 }
